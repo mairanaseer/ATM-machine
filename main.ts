bc45b4d6a2c821a927150b7c4c000e6b7d62f4b4
maira naseer ud din <161260376+mairanaseer@users.noreply.github.com>
2024-04-03 10:49:59 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,81 @@
+import inquirer from "inquirer";
+
+let myBalance = 10000;//dollar
+
+
+let mypin = 123456;
+
+let pinAnswer = await inquirer.prompt(
+   [ 
+    {
+    name: "pin",
+    message: "enter your pin",
+    type: "number",
+    }   
+]
+);
+
+if (pinAnswer.pin === mypin) {
+    console.log("correct pin code!!!");
+
+   let operationAns= await inquirer.prompt(
+        [
+            {
+                name:"operation",
+                message: "please select option",
+                type: "list",
+                choices: ["withdraw","check balance"]
+            }
+        ]
+    );
+    if (operationAns.operation === "withdraw") {
+          let amountAns = await inquirer.prompt(
+            [
+                {
+                    name: "amount",
+                    message: "enter your amount",
+                    type: "number"
+                }
+            ]
+          );
+          myBalance -= amountAns.amount;
+          console.log ("your remaining balance is:" + myBalance)
+
+                  
+    }else if (operationAns.operation === "check balance"){
+        console.log("your balance is:" + myBalance)
+    }
+}
+else{
+    console.log("incorrect pin code");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
